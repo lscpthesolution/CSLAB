13fd83d86a34501778f95604807985e0a8fbe6f7
Adam <adam.oh@libertas-scholars.org>
2024-11-05 10:18:22 +0900

    updated LAB01

---
+++ Ex02/Sample03.py
@@ -0,0 +1,19 @@
+'''
+Check if it is a triangle or not
+Print out the results and return nothing
+ex)
+True
+True
+False
+False
+True
+True
+
+'''
+triangleList = [(3,3,3), (3,4,5), (5,6,1), (4,5,9), (9,3,1), (3,5,8)]
+
+def checker(givenList: list) -> None:
+    # show your work
+    pass
+
+
