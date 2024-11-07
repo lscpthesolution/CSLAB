9445371336de37638ab8010a0f4f96fbd61e9680
Adam <adam.oh@libertas-scholars.org>
2024-11-07 09:36:11 +0900

    updated

---
+++ Ex02/Sample04.py
@@ -0,0 +1,6 @@
+s = set()                   # {}
+s.add(4)
+s.add(5)
+print(s)
+s.add(4)
+print(s)
---
+++ Ex02/Sample05.py
@@ -0,0 +1,16 @@
+# Dictionary
+d = dict()
+d = {"Adam": "Teacher", "Devyn":"Student", "Seoyeog":"Girl"}
+print(d)
+
+for each in d:
+    print(each)
+
+for each in d.keys():
+    print(each)
+
+for each in d.values():
+    print(each)
+
+
+
