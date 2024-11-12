badcc4dead210b26b554f7f9a2a1fe90308c5faf
Adam <adam.oh@libertas-scholars.org>
2024-11-12 09:41:29 +0900

    Sample02

---
+++ Ex03/Sample02.py
@@ -0,0 +1,34 @@
+names = ["Daeju", "Richard", "Kevin", "James", "Warren", "Hwanmin", "Donghyuk", "Taehoon", "Taeyoon", "Beomkyu", "Sieon", "Devyn", "Seoyoung", "Hayoung"]
+bDays = ["05_07_06", "05_12_13", "07_08_17", "06_08_25", "06_09_27", "06_05_23", "07_10_01", "06_02_01", "06_07_05", "07_06_02", "06_03_17", "05_11_17", "06_04_03", "07_07_11"]
+
+d = dict(zip(names, bDays))
+# print(d)
+# for each in d.items():
+#     print(each)
+
+
+
+
+
+
+
+
+
+
+# Please complete the functions below
+
+'''
+print out the names in the dictionary 
+'''
+def printNames(target : dict) -> None:
+    pass
+
+
+
+'''
+print out their bDays in the dictionary
+'''
+
+def printBDays(target : dict) -> None :
+    pass
+
