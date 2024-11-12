ddfcb6744acecc0fc47a075f4a7486c2c6c7cbaa
Adam <adam.oh@libertas-scholars.org>
2024-11-12 10:11:41 +0900

    provided a cslab

---
+++ Ex03/CSLab01.py
@@ -0,0 +1,14 @@
+names = ["Daeju", "Richard", "Kevin", "James", "Warren", "Hwanmin", "Donghyuk", "Taehoon", "Taeyoon", "Beomkyu", "Sieon", "Devyn", "Seoyoung", "Hayoung"]
+bDays = ["05_07_06", "05_12_13", "07_08_17", "06_08_25", "06_09_27", "06_05_23", "07_10_01", "06_02_01", "06_07_05", "07_06_02", "06_03_17", "05_11_17", "06_04_03", "07_07_11"]
+
+d = dict(zip(names, bDays))
+
+
+# find the oldest kid. Return his/her name
+def finder01(target : dict) -> str:    
+    pass
+
+
+# find the youngeset kid. Return his/her name
+def finder02(taret : dict) -> str:
+    pass
