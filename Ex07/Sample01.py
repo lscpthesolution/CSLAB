1047fa88d92a106e19b1189d30883dae4b01fd49
Minhyuk Adam Oh <adam.oh@libertas-scholars.orgclera>
2024-11-21 11:37:45 +0900

    CSLAB IS UP

---
+++ Ex07/Sample01.py
@@ -0,0 +1,15 @@
+import math
+
+
+def circumference(radius:float) -> float:
+    '''
+    calculate the circumference with the given radius.
+    return the result with two decial places.
+    use round up.
+    '''
+    
+    # remove this and the pass below before you start coding :) Have a fun!
+    pass
+
+
+
