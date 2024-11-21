f4122c97a91996ee854246501dde056986e19b80
Minhyuk Adam Oh <adam.oh@libertas-scholars.orgclera>
2024-11-21 11:41:31 +0900

    CSLAB_Ex07

--- Ex07/Sample01.py
+++ Ex07/Sample01.py
@@ -5,9 +5,11 @@ def circumference(radius:float) -> float:
     '''
     calculate the circumference with the given radius.
     return the result with two decial places.
-    use round up.
-    '''
+    you may want to have a helper function to make it work
     
+    Tip: use round up function and math.pi 
+    '''
+    pi = math.pi
     # remove this and the pass below before you start coding :) Have a fun!
     pass
 
